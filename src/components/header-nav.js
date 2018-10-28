6c44562a4d214e9d7d9881fd32cb06a360c5ec86
Allard van Helbergen <allard@vanhelbergen.com>
2018-10-28 16:28:01 +1100

    Refactor styles to styled-components.

--- src/components/header-nav.js
+++ src/components/header-nav.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const NavContainer = styled.div`
+  background: rebeccapurple;
+  marginbottom: 1.45rem;
+`
+
+const NavLeftContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const HomeLink = styled.h1`
+  margin: 0;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          vanhelbergen.com
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <NavContainer>
+    <NavLeftContainer>
+      <HomeLink>
+        <Link to="/">vanhelbergen.com</Link>
+      </HomeLink>
+    </NavLeftContainer>
+  </NavContainer>
 )
 
 export default Header
