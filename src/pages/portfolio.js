c10b5a68ba73e61a6cf6fa2413237ec0e4f10971
Allard van Helbergen <allard@vanhelbergen.com>
2020-03-02 13:41:15 +1100

    Add placholder pages for the other menu items.

---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+
+import Layout from '../layouts/default-layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Allard's portfolio</h1>
+    <p>Welcome to my portfolio.</p>
+  </Layout>
+)
+
+export default SecondPage
---
+++ src/pages/speaking.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+
+import Layout from '../layouts/default-layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Speaking</h1>
+    <p>map through speaking entries.</p>
+  </Layout>
+)
+
+export default SecondPage
