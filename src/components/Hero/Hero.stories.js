9c6bda5f7893fd96ea28926370cc359cb3a7111b
Allard van Helbergen <allard@vanhelbergen.com>
2020-04-10 22:27:09 +1000

    Add Hero story.

---
+++ src/components/Hero/Hero.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './Hero'
+
+export const All = () => (
+  <div>
+    <Hero>Children of the hero.</Hero>
+  </div>
+)
+
+export default {
+  title: 'Components/Hero',
+  component: Hero,
+}
