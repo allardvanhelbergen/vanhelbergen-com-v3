71f93304ea7021bc2d26d1ebc7fefc27b7b870d9
Allard van Helbergen <allard@vanhelbergen.com>
2020-04-17 00:02:05 +1000

    Add tailwind reset to global styles and remove duplicate styles.

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -1,133 +1,69 @@
 import { createGlobalStyle } from 'styled-components'
-
+import tw from 'twin.macro'
 import tokens from './tokens'
 import bodyBackgroundImage from '../images/noise-100x100-90noise-4opacity.png'
 
-const GlobalStyles = createGlobalStyle`
-  /* Box sizing rules */
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-  
-  /* Remove default padding */
-  ul[class],
-  ol[class] {
-    padding: 0;
-  }
-  
-  /* Remove default margin */
-  body,
-  h1,
-  h2,
-  h3,
-  h4,
-  p,
-  ul[class],
-  ol[class],
-  li,
-  figure,
-  figcaption,
-  blockquote,
-  dl,
-  dd {
-    margin: 0;
-  }
+/**
+ * Preflight: An opinionated set of base styles for Tailwind projects.
+ * Built on top of normalize.css
+ * https://tailwindcss.com/docs/preflight/
+ * https://unpkg.com/tailwindcss@1.2.0/dist/base.css
+ */
+import './tailwind.css'
 
+const GlobalStyles = createGlobalStyle`
   html {
-    -webkit-text-size-adjust: 100%;
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    font-size: 100%
   }
 
   /* Set core body defaults */
   body {
     min-height: 100vh;
+    width: 100vw;
+    max-width: 100vw;
     scroll-behavior: smooth;
     background: url(${bodyBackgroundImage});
     background-color: ${tokens.color.background};
     color: ${tokens.color.text.default};
-    font-family: ${tokens.font.base.family};
-    font-size: ${tokens.font.base.size};
-    font-weight: 400;
-    line-height: ${tokens.font.base.lineHeight};
     -webkit-font-smoothing: antialiased;
-    text-rendering: optimizeSpeed;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+    text-rendering: optimizeLegibility;
+    -webkit-font-feature-settings: "pnum";
+    font-feature-settings: "pnum";
+    font-variant-numeric: proportional-nums;
   }
 
   [tabindex="-1"]:focus {
     outline: 0 !important;
   }
 
-  /* A elements that don't have a class get default styles */
-  a:not([class]) {
-    text-decoration-skip-ink: auto;
+  h1 {
+    ${tw`text-6xl`}
+  }
+
+  h2 {
+    ${tw`text-4xl`}
   }
 
   a {
     color: ${tokens.color.text.link};
     background-color: transparent;
-  }
+    
+    &:hover {
+      color: ${tokens.color.text.linkHover};
+      text-decoration: none;
+    }
 
-  a:hover {
-    color: ${tokens.color.text.linkHover};
-    text-decoration: none;
+    /* A elements that don't have a class get default styles */
+    &:not([class]) {
+      text-decoration-skip-ink: auto;
+    }
   }
 
   /* Natural flow and rhythm in articles by default */
   article > * + * {
     margin-top: 1rem;
-  }
-
-  address {
-    margin-bottom: 1rem;
-    font-style: normal;
-    line-height: inherit;
-  }
-
-  pre,
-  code,
-  kbd,
-  samp {
-    font-family: ${tokens.font.code.family};
-    font-size: ${tokens.font.code.size};
-  }
-
-  pre {
-    overflow: auto;
-  }
-
-  /* Make images easier to work with */
-  img {
-    max-width: 100%;
-    display: block;
-  }
-
-  input,
-  button,
-  select,
-  optgroup,
-  textarea {
-    font-family: inherit;
-    font-size: inherit;
-    line-height: inherit;
-  }
-
-  textarea {
-    overflow: auto;
-    resize: vertical;
-  }
-
-  /* Remove list styles on ul, ol elements with a class attribute */
-  ul[class],
-  ol[class] {
-    list-style: none;
-  }
-
-  /* Utility styles */
-  [hidden] {
-    display: none !important;
   }
 
   /* Remove all animations and transitions for people that prefer not to see them */
